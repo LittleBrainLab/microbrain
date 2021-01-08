3b1ff4bb1e7e484c8bb2c9a8d0307ae4881ad1c1
Graham Little <gtlittle@ualberta.ca>
2021-01-07 22:20:50 -0700

    preprocessing/modelling v1.0

--- scripts/microbrain.py
+++ scripts/microbrain.py
@@ -5,6 +5,7 @@ import glob
 from os import system
 from os import path
 from time import time
+import subprocess
 
 import numpy as np
 import nibabel as nib
@@ -23,6 +24,13 @@ import mbrain_modelling as mbrain_modelling
 
 #sys.path.append('../surfing/')
 #import mbrain_cortical_segmentation as mbrain_cort
+def fsl_ext():
+    fsl_extension = ''
+    if os.environ['FSLOUTPUTTYPE'] == 'NIFTI':
+        fsl_extension = '.nii'
+    elif os.environ['FSLOUTPUTTYPE'] == 'NIFTI_GZ':
+        fsl_extension = '.nii.gz'
+    return fsl_extension
 
 def main(argv):
     inputDir = ''
@@ -30,6 +38,9 @@ def main(argv):
     bval_list =[]
     
     mask = False
+    fval = 0.15
+    gval = 0.1
+    rerun_mask = False
     denoise = False
     dnnl = False
     gibbs = False
@@ -37,7 +48,7 @@ def main(argv):
     cuda = False
     json = True
     dti_model = False
-    vb_seg = False
+    subcort_seg = False
     cort_seg = False
     N4 = True # By default do N4 correction on DWI image based on the bias field from b0
 
@@ -46,28 +57,34 @@ def main(argv):
     Note that options below require the installation of multiple neuroimaging/python analysis software packages (see install instructions).
     VERY BETA Version use with extreme caution. For trouble shooting help contact Graham Little (gtlittle@ualberta.ca)
 
-    options: 
-    -i <dicom_directory>,--idcm= - uses dcm2niix to convert dicom files to NIFTI which are then placed in a newly created subject directory
+    mandatory arguments:
     -s <directory>,--subdir= - specifies the directory which microbrain will place all processed files
-    -b [bval_list],--bvalues= - specify the bvalues to use for diffusion modelling and segmention (for example [0,1000] to use all b0 and b1000 volumes) 
+    -b [bval_list],--bvalues= - specify the bvalues to use for diffusion modelling and segmention (for example [0,1000] to use all b0 and b1000 volumes)
+
+    optional arguments: 
+    -i <dicom_directory>,--idcm= - uses dcm2niix to convert dicom files to NIFTI which are then placed in a newly created subject directory
+    --mask-params=[f-value,g-value], --- sets the bet2 mask parameters default [0.15,0.1]
+    --rerun-mask - deletes any previously created files (post-masking step) and reruns entire analysis using specified mask parameters 
     --gibbs - perform gibbs ringing correction (Kellner et al., 2016) using included third party c program
     --denoise - performs MPPCA denoising (Veraart et al., 2016) via DIPY
     --dnnl - performs Non-Local means denoising (Coupe et al., 2011) via DIPY
     --eddy, --eddy_cuda - performs eddy current correction (Anderson et al., 2016) via FSL v6.0 or higher. CUDA mode runs faster if CUDA setup on NVIDIA GPU
     --no-N4 - does not perform N4 correction (Tustison el al., 2010) via ANTS, useful flag when data is prescan normalized (Siemens)
     --no-json - if no .json image acquistion specification file available will run without this file
-    --subcort_seg - perform surface based subcortical segmentation (Little et al., 2021, ISMRM) via MIRTK
-    --cort_seg - perform surface based cortical segmentation (Little et al., 2021, NeuroImage) via MIRTK
+    --subcort-seg - perform surface based subcortical segmentation (Little et al., 2021, ISMRM) via MIRTK
+    --cort-seg - perform surface based cortical segmentation (Little et al., 2021, NeuroImage) via MIRTK
     
     packaged_pipelines
     --cb - (used for CB_BRAIN data) 1) eddy_cuda 2) modelling tensor 3) subcortical segmentation 4) cortical segmentation 
-        example: microbrain.py -s somepath/CB_BRAIN_050 -b [0,1000] --cb
+        example: python3 microbrain.py -s somepath/CB_BRAIN_050 -b [0,1000] --cb
     --all - (used for ALB300 data) 1) gibbs ringing correction 2) eddy_cuda 3) subcortical segmenation 4) cortical segmentation
-        example: microbrain.py -i Ab300_005/study/DTI_1p5mm...45b2500_12/ -s somepath/AB300_005 -b [0,1000] --all"""
+        example: python3 microbrain.py -i Ab300_005/study/DTI_1p5mm...45b2500_12/ -s somepath/AB300_005 -b [0,1000] --all
+    --rerun-mask, --mask-params - used when masking fails and pipeline needs to be rerun 
+        example: python3 microbrain.py -s somepath/Ab300_0005 -b [0,1000] --all --mask-params=[0.4,0.1] --rerun-mask"""
 
     try:
         # Note some of these options were left for testing purposes
-        opts, args = getopt.getopt(argv,"hs:b:i:",["idcm=","subdir=","bvalues=", "denoise","dnnl", "gibbs", "eddy", "eddy_cuda", "no-json","no-N4","all","segall", "hcp", "cb","allxeddy", "allxn4","allxcort"])
+        opts, args = getopt.getopt(argv,"hs:b:i:",["idcm=","subdir=","bvalues=", "mask-params=", "rerun-mask", "denoise","dnnl", "gibbs", "eddy", "eddy_cuda", "no-json","no-N4","all","subcort-seg","cort-seg", "hcp", "cb","allxeddy", "allxn4"])
     except getopt.GetoptError:
         print(help_string)
         sys.exit(2)
@@ -91,7 +108,13 @@ def main(argv):
             shell_suffix = ''
             for shell in bval_list:
                 shell_suffix = shell_suffix + 'b' + str(shell)
-
+        elif opt in ("--mask-params"):
+            mask = True
+            maskval_list = np.fromstring(arg.strip('[]'), np.double, sep=',')
+            fval = maskval_list[0]
+            gval = maskval_list[1]
+        elif opt in ("--rerun-mask"):
+            rerun_mask = True
         elif opt in ("--denoise"):
             denoise = True
         elif opt in ("--dnnl"):
@@ -111,8 +134,9 @@ def main(argv):
             json = False
         elif opt in ("--no-N4"):
             N4 = False
-        elif opt in ("--segall"):
-            vb_seg = True
+        elif opt in ("--subcort-seg"):
+            subcort_seg = True
+        elif opt in ("--cort-seg"):
             cort_seg = True
         elif opt in ("--all"):
             mask = True
@@ -120,48 +144,23 @@ def main(argv):
             eddy = True
             cuda = True
             dti_model = True
-            vb_seg = True
+            subcort_seg = True
             cort_seg = True
             N4 = True
-        elif opt in ("--allxcort"):
-            mask = True
-            gibbs = True
-            eddy = True
-            cuda = True
-            dti_model = True
-            vb_seg = True
-            cort_seg = False
-            N4 = True
-        elif opt in ("--cb"): # CB_BRAIN Preproc/DTI Maps
+        elif opt in ("--cb"): 
             mask = True
             eddy = True
             cuda = True
             dti_model = True
-            vb_seg = True
+            subcort_seg = True
             cort_seg = True
             N4 = True
         elif opt in("--hcp"):
             mask = True
             dti_model = True
-            vb_seg = True
+            subcort_seg = True
             cort_seg = True
             N4 = True
-        elif opt in ("--allxeddy"):
-            mask=True
-            gibbs = True
-            dti_model = True
-            vb_seg = True
-            cort_seg = True
-            N4 = True
-        elif opt in ("--allxn4"):
-            mask=True
-            gibbs = True
-            eddy = True
-            cuda = True
-            N4 = False
-            dti_model = True
-            vb_seg = True
-            cort_seg = True
 
     outputDir, subID = os.path.split(outputDir)
     print('Processing:' + subID)
@@ -177,11 +176,16 @@ def main(argv):
         print('DSurfer: dcm2niix returned an error, make sure it is installed correctly and that dicom files exist')
         sys.exit()
 
-
+    # Naming convention for output directories
     origDir = outputDir + '/' + subID + '/orig/'
-    
-    ## Denoising/Gibbs ringing correction
     preprocDir = outputDir + '/' + subID + '/preprocessed/'
+    meanDWIDir = outputDir + '/' + subID + '/meanDWI/'
+    tensorDir = outputDir + '/' + subID + '/DTI_maps/'
+    regDir = outputDir + '/' + subID + '/registration/'
+    subcortDir = outputDir + '/' + subID + '/subcortical_segmentation/'
+    cortDir = outputDir + '/' + subID + '/cortical_segmentation/'
+
+    ## Denoising/Gibbs ringing correction
     fout = fdwi
     preproc_suffix = ''
     if denoise:
@@ -199,7 +203,22 @@ def main(argv):
     # mask data using meanB0
     bvals, bvecs = read_bvals_bvecs(fbval, fbvec)
     if mask:
-        fmask, fb0avg, stdout, returncode = mbrain_preproc.fslv6p0_brain_mask(fout,bvals)
+        # If rerun mask flag, delete all data analyzed post mask and rerun all analysis
+        if rerun_mask:
+            for thisDir in [meanDWIDir, tensorDir, regDir, subcortDir, cortDir, preprocDir + 'eddy_output_files/']:
+                if os.path.exists(thisDir):
+                    process = subprocess.run(['rm', '-r', thisDir], stdout=subprocess.PIPE, universal_newlines=True)
+            
+            feddy = fout.replace(fsl_ext(), '_EDDY' + fsl_ext())
+            if os.path.exists(feddy):
+                process = subprocess.run(['rm', feddy], stdout=subprocess.PIPE, universal_newlines=True)
+                process = subprocess.run(['rm', feddy + '.eddy_rotated_bvecs'], stdout=subprocess.PIPE, universal_newlines=True)
+            
+            fmask = fout.replace(fsl_ext(), '_b0avg_BET_mask' + fsl_ext())
+            if os.path.exists(fmask):
+                process = subprocess.run(['rm', fmask], stdout=subprocess.PIPE, universal_newlines=True)
+
+        fmask, fb0avg, stdout, returncode = mbrain_preproc.fslv6p0_brain_mask(fout,bvals,fval,gval)
         if returncode != 0:
             print('DSurfer: bet2 returned an error, make sure it is installed correctly')
             sys.exit()
@@ -259,8 +278,8 @@ def main(argv):
         # Output Tensor and associated diffusion parametric maps
         ffa, fmd = mbrain_modelling.output_DTI_maps_multishell(fout, fmask, bvals, bvecs, tensorDir, shells = bval_list)
         
-        # Will be Implementing this soon
-        #if vb_seg:
+        # Will be Porting over my new code into this soon
+        #if subcort_seg:
         #    mbrain_seg.register_and_segment(fb0_n4, fdwi_n4, ffa, fmask, outputDir, subID, suffix)
 
         # Will be finishing implementing this soon
