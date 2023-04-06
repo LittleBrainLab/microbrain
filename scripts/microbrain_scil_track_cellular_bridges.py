972e8929a298c9261cc5f23bf9014f467343fd89
grahamlittlephd <grahamlittlephd@gmail.com>
2023-04-06 10:03:43 -0400

    versions #1 gm cell bridge tractography

--- scripts/microbrain_scil_track_cellular_bridges.py
+++ scripts/microbrain_scil_track_cellular_bridges.py
@@ -34,7 +34,7 @@ def fsl_ext():
     return fsl_extension
 
 
-def run_cell_bridge_tractography(subDir, subID, outDir, algo='prob', nbr_seeds=10, sfthres_init=0.1, sfthres=0.15, min_length=20, min_mesh_length=0.10, max_mesh_length=0.15):
+def run_cell_bridge_tractography(subDir, subID, outDir, algo='prob', nbr_seeds=10, sfthres_init=0.1, sfthres=0.15, min_length_tractogram=0, min_length=4, max_length=20, min_mesh_length=0.10, max_mesh_length=0.15):
 
     mask_dir = outDir + '/tracking_mask'
     if not os.path.exists(mask_dir):
@@ -93,7 +93,16 @@ def run_cell_bridge_tractography(subDir, subID, outDir, algo='prob', nbr_seeds=1
     if not os.path.exists(tractogram_dir):
         os.makedirs(tractogram_dir)
 
+    tractogram_gm_cellular_bridge_dir = tracking_dir + '/tractograms_gm_cellular_bridges'
+    if not os.path.exists(tractogram_gm_cellular_bridge_dir):
+        os.makedirs(tractogram_gm_cellular_bridge_dir)
+
+    metric_dir = tracking_dir + '/metric_maps'
+    if not os.path.exists(metric_dir):
+        os.makedirs(metric_dir)
+
     # for each hemisphere left and right create a tractogram for the striatum and globus pallidus
+    # Generating globus tractogram here just for QA purposes (is not included in gm cell bridge reconstruction)
     for hemi in ['LEFT', 'RIGHT']:
         for struct in ['STRIATUM', 'GLOBUS']:
             StructVTK=subDir + '/subcortical_segmentation/mesh_output/' + subID + '_refined_' + hemi + '_' + struct + '.vtk' 
@@ -104,30 +113,36 @@ def run_cell_bridge_tractography(subDir, subID, outDir, algo='prob', nbr_seeds=1
 
             StructPLY_remesh = StructPLY.replace('.ply','_remesh.ply')
             if not os.path.exists(StructPLY_remesh):
-                os.system('mirtk remesh-surface ' + StructPLY + ' ' + StructPLY_remesh + ' -min_edgelength ' + str(min_mesh_length) + ' -max_edgelength ' + str(max_mesh_length))
+                os.system('mirtk remesh-surface ' + StructPLY + ' ' + StructPLY_remesh + ' -min-edgelength ' + str(min_mesh_length) + ' -max-edgelength ' + str(max_mesh_length))
             
             # Output txt files containing mesh vertex coordinates and norms
             inCoords_remesh = seed_dir + '/' + subID + '_refined_' + hemi + '_' + struct + '_remesh_coords.txt'
             inNorms_remesh = seed_dir + '/' + subID + '_refined_' + hemi + '_' + struct + '_remesh_norms.txt'
             StructPLY_remesh_reorient=mesh_dir + '/' + subID + '_refined_' + hemi + '_' + struct + '_remesh_reorient.ply'
             if not os.path.exists(inCoords_remesh):
-                os.system('python scil_convert_mesh_to_coords_norms.py ' + StructPLY_remesh + ' ' + inCoords_remesh + ' ' + inNorms_remesh + ' --apply_transform ' + fmask_brain + ' --ras --output_mesh ' + StructPLY_remesh_reorient)
+                os.system('scil_convert_mesh_to_coords_norms.py ' + StructPLY_remesh + ' ' + inCoords_remesh + ' ' + inNorms_remesh + ' --apply_transform ' + fmask_brain + ' --ras --output_mesh ' + StructPLY_remesh_reorient)
 
             # Generate tractogram for structure
             out_tractogram_remesh = tractogram_dir + '/' + subID + '_refined_' + hemi + '_' + struct + '_tractogram.trk'
             if not os.path.exists(out_tractogram_remesh):
                 os.system('scil_compute_mesh_based_local_tracking.py ' + ffodf + ' ' + inCoords_remesh + ' ' +
-                        fmask_brain + ' ' + out_tractogram_remesh + ' --in_norm_list ' + inNorms_remesh +
-                        ' --save_seeds --min_length ' + str(min_length) + ' --sfthres_init ' + str(sfthres_init) + ' --sfthres ' + str(sfthres) + ' --algo ' + str(algo) + ' --nbr_sps ' + str(nbr_seeds) + ' -v ')
+                        fmask_tracking + ' ' + out_tractogram_remesh + ' --in_norm_list ' + inNorms_remesh +
+                        ' --save_seeds --min_length ' + str(min_length_tractogram) + ' --sfthres_init ' + str(sfthres_init) + ' --sfthres ' + str(sfthres) + ' --algo ' + str(algo) + ' --nbr_sps ' + str(nbr_seeds) + ' -v ')
     
     # Filter tractograms to create a single cellular bridge tractogram per hemisphere
+    streamline_file_list = []
     for hemi in ['LEFT', 'RIGHT']:
-        # STRIATUM Both Ends
+        # tractograms
         in_tractogram_STRIATUM = tractogram_dir + '/' + subID + '_refined_' + hemi + '_STRIATUM_tractogram.trk'
+
+        # Meshes
         in_mesh_STRIATUM = mesh_dir + '/' + subID + '_refined_' + hemi + '_STRIATUM_remesh.ply'
-        out_tractogram_STRIATUM_STRIATUM = tractogram_dir + '/' + subID + '_refined_' + hemi + '_STRIATUM_tractogram_filter_STRIATUM_Both_Ends.trk'
+        in_mesh_GLOBUS = mesh_dir + '/' + subID + '_refined_' + hemi + '_GLOBUS_remesh.ply'
+
+        # STRIATUM Both Ends
+        out_tractogram_STRIATUM_STRIATUM = tractogram_gm_cellular_bridge_dir + '/' + subID + '_refined_' + hemi + '_STRIATUM_tractogram_filter_STRIATUM_Both_Ends.trk'
         if not os.path.exists(out_tractogram_STRIATUM_STRIATUM):
-            os.system('scil_filter_tractogram_based_on_mesh.py ' + in_tractogram_STRIATUM + ' ' + out_tractogram_STRIATUM_STRIATUM + ' --mesh_roi ' + in_mesh_STRIATUM + ' both_ends include')
+            os.system('scil_filter_tractogram_based_on_mesh.py ' + in_tractogram_STRIATUM + ' ' + out_tractogram_STRIATUM_STRIATUM + ' --mesh_roi ' + in_mesh_STRIATUM + ' both_ends include --dist_thr 0.1')
         
         # STRIATUM orientation
         out_tractogram_STRIATUM_STRIATUM_orientation = out_tractogram_STRIATUM_STRIATUM.replace('.trk', '_orientationMaxY3.trk')
@@ -135,19 +150,49 @@ def run_cell_bridge_tractography(subDir, subID, outDir, algo='prob', nbr_seeds=1
             os.system('scil_filter_streamlines_by_orientation.py ' + out_tractogram_STRIATUM_STRIATUM + ' ' + out_tractogram_STRIATUM_STRIATUM_orientation + ' --max_y 3 ')
 
         # STRIATUM length
-        out_tractogram_STRIATUM_STRIATUM_orientation_length = out_tractogram_STRIATUM_STRIATUM_orientation.replace('.trk', '_maxlen20.trk')
+        out_tractogram_STRIATUM_STRIATUM_orientation_length = out_tractogram_STRIATUM_STRIATUM_orientation.replace('.trk', '_filterlen.trk')
         if not os.path.exists(out_tractogram_STRIATUM_STRIATUM_orientation_length):
-            os.system('scil_filter_streamlines_by_length.py ' + out_tractogram_STRIATUM_STRIATUM_orientation + ' ' + out_tractogram_STRIATUM_STRIATUM_orientation_length + ' --maxL 20')
-        
-        # GLOBUS end in STRIATUM
-
+            os.system('scil_filter_streamlines_by_length.py ' + out_tractogram_STRIATUM_STRIATUM_orientation + ' ' + out_tractogram_STRIATUM_STRIATUM_orientation_length + ' --maxL ' + str(max_length) + ' --minL ' + str(min_length))
+        streamline_file_list.append(out_tractogram_STRIATUM_STRIATUM_orientation_length)
 
         # STRIATUM end in GLOBUS
+        out_tractogram_STRIATUM_GLOBUS = tractogram_gm_cellular_bridge_dir + '/' + subID + '_refined_' + hemi + '_STRIATUM_tractogram_filter_GLOBUS_End.trk'
+        if not os.path.exists(out_tractogram_STRIATUM_GLOBUS):
+            os.system('scil_filter_tractogram_based_on_mesh.py ' + in_tractogram_STRIATUM + ' ' + out_tractogram_STRIATUM_GLOBUS + ' --mesh_roi ' + in_mesh_GLOBUS + ' only_end include --dist_thr 0.1')
+        
+        # STRIATUM orientation
+        out_tractogram_STRIATUM_GLOBUS_orientation = out_tractogram_STRIATUM_GLOBUS.replace('.trk', '_orientationMaxY3.trk')
+        if not os.path.exists(out_tractogram_STRIATUM_GLOBUS_orientation):
+            os.system('scil_filter_streamlines_by_orientation.py ' + out_tractogram_STRIATUM_GLOBUS + ' ' + out_tractogram_STRIATUM_GLOBUS_orientation + ' --max_y 3 ')
+        
+        # STRIATUM length
+        out_tractogram_STRIATUM_GLOBUS_orientation_length = out_tractogram_STRIATUM_GLOBUS_orientation.replace('.trk', '_filterlen.trk')
+        if not os.path.exists(out_tractogram_STRIATUM_GLOBUS_orientation_length):
+            os.system('scil_filter_streamlines_by_length.py ' + out_tractogram_STRIATUM_GLOBUS_orientation + ' ' + out_tractogram_STRIATUM_GLOBUS_orientation_length + ' --maxL ' + str(max_length) + ' --minL ' + str(min_length))
+        streamline_file_list.append(out_tractogram_STRIATUM_GLOBUS_orientation_length)
 
+        # concatentate tractograms
+        out_tractogram_cellular_bridge = tractogram_gm_cellular_bridge_dir + '/' + subID + '_refined_' + hemi + '_gm_celluar_bridge_tractogram.trk'
+        if not os.path.exists(out_tractogram_cellular_bridge):
+            os.system('scil_streamlines_math.py concatenate ' + out_tractogram_STRIATUM_STRIATUM_orientation_length +
+                      ' ' + out_tractogram_STRIATUM_GLOBUS_orientation_length +
+                      ' ' + out_tractogram_cellular_bridge)    
+        streamline_file_list.append(out_tractogram_cellular_bridge)
 
     # Output quantative metrics for each tractogram
+    for streamline_file in streamline_file_list:
+        streamline_file_name = streamline_file.split('/')[-1]
+        out_streamline_density = metric_dir + '/' + streamline_file_name.replace('.trk', '_density.nii.gz')
+        if not os.path.exists(out_streamline_density):
+            os.system('scil_compute_streamlines_density_map.py ' + streamline_file + ' ' + out_streamline_density)
 
-
+        out_streamline_density_norm = metric_dir + '/' + streamline_file_name.replace('.trk', '_density_norm.nii.gz')
+        if not os.path.exists(out_streamline_density_norm):
+            os.system('scil_image_math.py normalize_max ' + out_streamline_density + ' ' + out_streamline_density_norm + ' --data float32')
+            
+        out_afd_map = metric_dir + '/' + streamline_file_name.replace('.trk', '_afd.nii.gz')
+        if not os.path.exists(out_afd_map):
+            os.system('scil_compute_mean_fixel_afd_from_bundles.py ' + streamline_file + ' ' + ffodf + ' ' + out_afd_map)
 
     return
 
@@ -186,7 +231,6 @@ def main(argv):
         elif opt in ("-o", "--outDir"):
             outDir = os.path.normpath(arg)
 
-    
     # Get FA, MD and subcortical GM segmentation directory
     baseDir, subID = os.path.split(os.path.normpath(subDir))
 
@@ -199,7 +243,7 @@ def main(argv):
     if not os.path.exists(outDir):
         os.makedirs(outDir)
 
-    run_wb_tractography(subDir, subID, outDir)
+    run_cell_bridge_tractography(subDir, subID, outDir)
     
 if __name__ == "__main__":
     main(sys.argv[1:])
